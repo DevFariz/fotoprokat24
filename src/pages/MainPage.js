3ecce4064caf489b4f7053f33174e19c7909bd89
R. Fariz <farizramazanov38@gmail.com>
2022-09-06 15:13:35 +0400

    feat: add news section at main page

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -3,6 +3,7 @@ import Gallery from "../components/gallery/Gallery";
 import Intro from "../components/intro/Intro";
 import IntroInfo from "../components/introInfo/IntroInfo";
 import MainSlider from "../components/mainSlider/MainSlider";
+import NewsMain from "../components/news-main/NewsMain";
 import Search from "../components/search/Search";
 
 const MainPage = () => {
@@ -15,6 +16,7 @@ const MainPage = () => {
       <Search />
       <Gallery />
       <EarnWithUs />
+      <NewsMain />
     </main>
   )
 }
